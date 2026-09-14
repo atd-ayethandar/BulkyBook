6166ab3dfa7535f6495437e68ddd920b22cf4dfe
atd-ayethandar <atd.ayethandar127@gmail.com>
2026-09-14 22:10:18 +0900

    Adding areas to project Adding layout to areas

--- BulkyBookWeb/Areas/Customer/Controllers/CategoryController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/CategoryController.cs
@@ -2,8 +2,9 @@
 using BulkyBook.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BulkyBookWeb.Controllers
+namespace BulkyBookWeb.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class CategoryController : Controller
     {
         private readonly ICategoryService _categoryService;
