1d69103e73d21aa35423ed8209b68683d57acf79
atd-ayethandar <atd.ayethandar127@gmail.com>
2026-09-15 22:19:51 +0900

    Create API Endpoint

--- BulkyBookWeb/Areas/Customer/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/ProductController.cs
@@ -92,5 +92,13 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
             return RedirectToAction("Index");
 
         }
+
+        #region API CALLS
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _productService.GetAllProductsAsync();
+            return Json(new { data = products });
+        }
+        #endregion
     }
 }
