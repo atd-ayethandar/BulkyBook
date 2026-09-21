1882ebc423e9832d7fd3f98d003383654dc5d989
atd-ayethandar <atd.ayethandar127@gmail.com>
2026-09-21 18:12:37 +0900

    Assign Role In Action

--- BulkyBook.Models/ViewModels/RegisterVM.cs
+++ BulkyBook.Models/ViewModels/RegisterVM.cs
@@ -35,7 +35,7 @@ namespace BulkyBook.Models.ViewModels
 
         [Display(Name = "Phone Number")]
         public string? PhoneNumber { get; set; }
-        public string Role { get; set;  }
+        public string? Role { get; set;  }
         [ValidateNever]
         public IEnumerable<SelectListItem> RoleList { get; set; }
     }
