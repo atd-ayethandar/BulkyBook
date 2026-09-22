0c98615f3b0ecaceaaeff74e100ccc01c6568d47
atd-ayethandar <atd.ayethandar127@gmail.com>
2026-09-22 15:51:56 +0900

    Load Shopping Cart on Get Endpoint

--- BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -1,9 +1,13 @@
 ﻿using BulkyBook.Business.Services.IServices;
+using BulkyBook.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace BulkyBookWeb.Areas.Customer.Controllers
 {
     [Area("Customer")]
+    [Authorize]
     public class CartController : Controller
     {
         private readonly IProductService _productService;
@@ -16,8 +20,26 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
 
         public async Task<IActionResult> Index()
         {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (string.IsNullOrEmpty(userId))
+            {
+                return Unauthorized();
+            }
 
-            return View();
+            var cartItems = await _shoppingCartService.GetUserCartItemsAsync(userId);
+
+            ShoppingCartVM shoppingCartVM = new()
+            {
+                ShoppingCartList = cartItems,
+                OrderHeader = new()
+            };
+
+            foreach (var cart in shoppingCartVM.ShoppingCartList)
+            {
+                shoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.Count);
+            }
+            return View(shoppingCartVM);
         }
     }
 }
