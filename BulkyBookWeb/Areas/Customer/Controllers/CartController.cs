9f8e6038ee701cca5b365aafeb4d15cb61fa59f0
atd-ayethandar <atd.ayethandar127@gmail.com>
2026-09-22 15:44:48 +0900

    Cart UI

---
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,23 @@
+﻿using BulkyBook.Business.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CartController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly IShoppingCartService _shoppingCartService;
+        public CartController(IProductService productService, IShoppingCartService shoppingCartService)
+        {
+            _productService = productService;
+            _shoppingCartService = shoppingCartService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            return View();
+        }
+    }
+}
